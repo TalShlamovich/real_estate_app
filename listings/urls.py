fa0d14be54d7070db2d6c554efa4392bd6a24a8d
TalShlamovich <talshlamovich@gmail.com>
2022-08-21 17:53:25 +0300

    first day

---
+++ listings/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import listing, listings, search
+
+urlpatterns = [
+    path('', listings, name='listings'),
+    path('<int:listing_id>', listing, name='listing'),
+    path('search', search, name='search'),
+]
