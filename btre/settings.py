3019730f603885322ce1b16193174c0a6e3c20a4
TalShlamovich <talshlamovich@gmail.com>
2022-08-21 22:51:28 +0300

    hidden secret key

--- btre/settings.py
+++ btre/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0pvqp_r=$%vr*!o&3tzwt277y=*$ppy4!k82-0lz320@%b#((v'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
