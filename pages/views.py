6ad4f14e6080760ec64b9056cf7466da53ee9f5b
TalShlamovich <talshlamovich@gmail.com>
2022-08-23 15:49:29 +0300

    start of stage 7

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,33 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Listing
+from realtors.models import Realtor
+from listings.choices import bedroom_choices, state_choices, price_choices
 
 # Create your views here.
 
 def home(request):
-    return render (request, '../templates/home.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings':listings,
+        # choices for search
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+    }
+    
+    
+    return render (request, '../templates/home.html', context)
 
 
 def about (request):
-    return render (request, '../templates/about.html')
+    realtors = Realtor.objects.all()
+
+    mvp = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors':realtors,
+        'mvp':mvp
+    }
+    return render (request, '../templates/about.html', context)
